7122310c9c0503eea6121ed248059cc526a157dd
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2024-11-10 22:27:14 +1100

    Updated Title 3

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -45,8 +45,8 @@ cat << EOM > /var/www/html/index.html
   <body>
     <div id="container">
       <h1>Terramino</h1>
+      <canvas width="320" height="640" id="game"></canvas>
     </div>
-    <canvas width="320" height="640" id="game"></canvas>
     <script>
       // https://tetris.fandom.com/wiki/Tetris_Guideline
 
