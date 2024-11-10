f83ce989c74a6c03072c09f2b782573408a44270
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2024-11-10 23:15:16 +1100

    Updated Title Dev

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -44,7 +44,7 @@ cat << EOM > /var/www/html/index.html
 
   <body>
     <div id="container">
-      <h1>Terramino</h1>
+      <h1>Terramino Demo</h1>
       <canvas width="320" height="640" id="game"></canvas>
     </div>
     <script>
