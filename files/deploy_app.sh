af34c4b9d041413908d889e3d9cd1bd713588e5a
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2024-11-12 11:17:59 +1100

    update title and instance size

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -44,7 +44,7 @@ cat << EOM > /var/www/html/index.html
 
   <body>
     <div id="container">
-      <h1>Adelaide HCP Workshop</h1>
+      <h1>Melbourne HCP Workshop</h1>
       <canvas width="320" height="640" id="game"></canvas>
     </div>
     <script>
